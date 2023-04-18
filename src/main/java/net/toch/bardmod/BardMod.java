5d7c5c0dce7bfbe0dc7f80dce7501effa5cf6fc5
eros <villarroel.eros@correo.unimet.edu.ve>
2023-04-18 00:01:50 -0700

    Initial commit

---
+++ src/main/java/net/toch/bardmod/BardMod.java
@@ -0,0 +1,14 @@
+package net.toch.bardmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BardMod implements ModInitializer {
+	public static final String MOD_ID = "bardmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/toch/bardmod/BardModClient.java
@@ -0,0 +1,10 @@
+package net.toch.bardmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BardModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
